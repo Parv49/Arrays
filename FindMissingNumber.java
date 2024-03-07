65a65e252c7296fa98b56c4551b5f036a0bd375f
Parv Gupta <149174587+Parv49@users.noreply.github.com>
2024-03-07 23:18:08 +0530

    Add files via upload

---
+++ FindMissingNumber.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class FindMissingNumber {
+    
+    public static void MissingNumber(int[]arr){
+        int n = arr.length+1;
+        int sum=n*(n+1)/2;
+        for(int i=0;i<arr.length;i++){
+            sum=sum-arr[i];
+        }
+        System.out.println(sum);
+    }
+    
+    public static void main(String[]args){
+        int[]arr={2,4,1,8,6,3,7};
+        MissingNumber(arr);
+    }
+    
+}
