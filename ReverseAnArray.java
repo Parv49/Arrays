3b53230a9e2fd217c9901726f4fa61a8f03fb437
Parv Gupta <149174587+Parv49@users.noreply.github.com>
2024-03-08 23:28:24 +0530

    Add files via upload

---
+++ ReverseAnArray.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.*;
+
+public class ReverseAnArray {
+    
+    public static void reverse(int []arr,int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    
+    public static void main(String[]args){
+        int[]arr={1,2,3,4,5,6};
+        reverse(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
